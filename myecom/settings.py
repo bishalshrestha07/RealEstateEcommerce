423b09f0e35807b2864f979e97daf87f057b5b66
bishal shrestha <biscro7@gmail.com>
2021-05-27 17:31:49 +0545

    customize the ADMIN app

--- myecom/settings.py
+++ myecom/settings.py
@@ -121,3 +121,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media Folder Settings
+MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_URL = '/media/'
