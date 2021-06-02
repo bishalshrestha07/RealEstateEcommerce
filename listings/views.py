cdea3d0a88c15d5045a06503ee1c60ea2eaf3892
bishal shrestha <biscro7@gmail.com>
2021-06-02 18:48:16 +0545

    work with dynamic data from database and pagination

--- listings/views.py
+++ listings/views.py
@@ -1,11 +1,24 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from .models import Listing
+from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
 
 def index(request):
-  return render(request,'listings/listings.html')
+  listings = Listing.objects.order_by('-list_date').filter(is_published=True)
 
-def listing(request):
-  return render(request,'listings/listing.html')
+  paginator = Paginator(listings, 6)
+  page = request.GET.get('page')
+  paged_listings = paginator.get_page(page)
+  context = {
+    'listings':paged_listings
+  }
+  return render(request,'listings/listings.html',context)
+
+def listing(request, listing_id):
+  listing = get_object_or_404(Listing, pk=listing_id)
+  context = {
+    'listing':listing
+  }
+  return render(request,'listings/listing.html',context)
 
 def search(request):
   return render(request,'listings/search.html')
